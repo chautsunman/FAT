d2d0b8aabd56f8f130f60bc1be94bc87247589db
Chau Tsun Man <tsunman.chau96@gmail.com>
2017-03-26 10:25:33 -0700

    Parse the boot sector

--- fatinfo.c
+++ fatinfo.c
@@ -4,8 +4,13 @@
 #include "fatinfo.h"
 #include <stdio.h>
 #include <stddef.h>
+#include <math.h>
 #include "directory.h"
 #include "disk.h"
+
+
+void parseFileSystemInfo(uint8_t *, filesystem_info *);
+
 
 /*
  * Function to print information about a FAT filesystem (useful for debugging).
@@ -47,9 +52,9 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
-  // Read the first sector of the disk to get the basic information abou the 
+  // Read the first sector of the disk to get the basic information abou the
   // filesystem on this disk.
-  
+
   uint8_t  buffer[DEFAULT_SECTOR_SIZE];
   if (readSectors(0, 1, buffer) != DEFAULT_SECTOR_SIZE) {
     char buff[256];
@@ -57,16 +62,37 @@ int main(int argc, char *argv[]) {
     perror(buff);
     return 0;
   }
-  
+
   // Make a call to Interpret the boot sector and fill in the  filesystem_info structure);
 
   // Add your code
-
+  parseFileSystemInfo(buffer, &fsInfo);
 
   print_filesystem_info(&fsInfo);
-  
-  
+
+
   // Add calls to function to print the directory tree
 
   return 0;
+}
+
+
+void parseFileSystemInfo(uint8_t *bootSector, filesystem_info *fsInfo) {
+  if (bootSector[17] == 0 && bootSector[18] == 0) {
+    fsInfo->fs_type = FAT32;
+    fsInfo->sector_size = bootSector[12] * 256 + bootSector[11];
+    fsInfo->cluster_size = bootSector[13];
+    fsInfo->rootdir_size = bootSector[18] * 256 + bootSector[17];
+    fsInfo->sectors_per_fat = bootSector[39] * 16777216 + bootSector[38] * 65536 + bootSector[37] * 256 + bootSector[36];
+    fsInfo->reserved_sectors = bootSector[15] * 256 + bootSector[14];
+    fsInfo->hidden_sectors = bootSector[31] * 16777216 + bootSector[30] * 65536 + bootSector[29] * 256 + bootSector[28];
+  } else {
+    fsInfo->fs_type = FAT12;
+    fsInfo->sector_size = bootSector[12] * 256 + bootSector[11];
+    fsInfo->cluster_size = bootSector[13];
+    fsInfo->rootdir_size = bootSector[18] * 256 + bootSector[17];
+    fsInfo->sectors_per_fat = bootSector[23] * 256 + bootSector[22];
+    fsInfo->reserved_sectors = bootSector[15] * 256 + bootSector[14];
+    fsInfo->hidden_sectors = bootSector[29] * 256 + bootSector[28];
+  }
 }
